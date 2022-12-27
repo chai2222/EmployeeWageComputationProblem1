0304e984c5f910cdade3b67a28800546a75d445f
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-27 16:39:21 +0530

    CheckingEmployeeAttendance

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeAttendance
+    {
+        public static void checkAttendance()
+        {
+            int is_Present = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if(empCheck==is_Present)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
