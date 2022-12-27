89ad79f454eb0e37e8149ba432e727cb527edc2f
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-27 16:25:51 +0530

    Greeting Massage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Problem");
+        }
+    }
+}
