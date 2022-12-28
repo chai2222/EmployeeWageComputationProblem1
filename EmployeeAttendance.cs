1a794ab8d2a67f67cd1077e79bfb5e8c8ce3ca9d
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-28 15:14:50 +0530

    EmployeeMothWage

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -7,43 +7,50 @@
         const int is_Wage_Per_Hour = 20;
 
         const int is_Part_Time = 2;
+        const int num_Of_Working_days = 20;
 
         int emp_Wage = 0;
         int emp_Hour = 0;
+        int total_Emp_Wage = 0;
 
         Random random = new Random();
         int empCheck = random.Next(3);
 
-        switch (empCheck)
+        for (int i = 0; i < num_Of_Working_days; i++)
         {
-            case is_Full_Time:
-                Console.WriteLine("Employee Is Present & Full Time");
-                emp_Hour = 8;
-                break;
-            case is_Part_Time:
-                Console.WriteLine("Employee Is Part Time");
-                emp_Hour = 4;
-                break;
-            case 0:
-                Console.WriteLine("Employee Is Absent");
-                break; 
-        }
-           /* if (empCheck == is_Full_Time)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee Is Present");
-                emp_Hour = 8;
+                case is_Full_Time:
+                    Console.WriteLine("Employee Is Present & Full Time");
+                    emp_Hour = 8;
+                    break;
+                case is_Part_Time:
+                    Console.WriteLine("Employee Is Part Time");
+                    emp_Hour = 4;
+                    break;
+                case 0:
+                    Console.WriteLine("Employee Is Absent");
+                    break;
             }
+            /* if (empCheck == is_Full_Time)
+             {
+                 Console.WriteLine("Employee Is Present");
+                 emp_Hour = 8;
+             }
 
-            else if(empCheck==is_Part_Time)
-            {
-                Console.WriteLine("Employee Is Part Time");
-                emp_Hour = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee Is Absent");
-            }*/
+             else if(empCheck==is_Part_Time)
+             {
+                 Console.WriteLine("Employee Is Part Time");
+                 emp_Hour = 4;
+             }
+             else
+             {
+                 Console.WriteLine("Employee Is Absent");
+             }*/
             emp_Wage = emp_Hour * is_Wage_Per_Hour;
+            total_Emp_Wage += emp_Wage;
             Console.WriteLine("Employee Wage :" + emp_Wage);
+        }
+        Console.WriteLine("Total Employee Wage is :" + total_Emp_Wage);
         }
     }
