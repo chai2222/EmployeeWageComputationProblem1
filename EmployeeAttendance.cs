91e24a658f087307153441456ddf8991cf6050f1
chai2222 <chaitanayajagadale6@gmail.com>
2022-12-28 14:57:01 +0530

    EmployeeWage

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -1,20 +1,34 @@
 ﻿
     public class EmployeeAttendance
     {
-        public static void checkAttendance()
+    public static void checkAttendance()
+    {
+        const int is_Full_Time = 1;
+        const int is_Wage_Per_Hour = 20;
+
+        const int is_Part_Time = 2;
+
+        int emp_Wage = 0;
+        int emp_Hour = 0;
+
+        Random random = new Random();
+        int empCheck = random.Next(3);
+
+        switch (empCheck)
         {
-            int is_Full_Time = 1;
-            int is_Wage_Per_Hour = 20;
-
-            int is_Part_Time = 2;
-            
-            int emp_Wage = 0;
-            int emp_Hour = 0;
-
-            Random random = new Random();
-            int empCheck = random.Next(3);
-
-            if (empCheck == is_Full_Time)
+            case is_Full_Time:
+                Console.WriteLine("Employee Is Present & Full Time");
+                emp_Hour = 8;
+                break;
+            case is_Part_Time:
+                Console.WriteLine("Employee Is Part Time");
+                emp_Hour = 4;
+                break;
+            case 0:
+                Console.WriteLine("Employee Is Absent");
+                break; 
+        }
+           /* if (empCheck == is_Full_Time)
             {
                 Console.WriteLine("Employee Is Present");
                 emp_Hour = 8;
@@ -25,12 +39,10 @@
                 Console.WriteLine("Employee Is Part Time");
                 emp_Hour = 4;
             }
-
-
             else
             {
                 Console.WriteLine("Employee Is Absent");
-            }
+            }*/
             emp_Wage = emp_Hour * is_Wage_Per_Hour;
             Console.WriteLine("Employee Wage :" + emp_Wage);
         }
